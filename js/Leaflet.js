b99358c4fab2756bbbd180785f26b11fb8f72da2
Joshua <grimrai@protonmail.com>
2022-03-10 15:09:01 +0000

    Edit onLocationFound

--- js/Leaflet.js
+++ js/Leaflet.js
@@ -20,22 +20,13 @@ function success(pos) {
 
 navigator.geolocation.getCurrentPosition(success);
 
-function onLocationFound(e) {
-	var radius = e.accuracy;
+function onLocationFound() {
 
-	L.marker(e.latlng).addTo(map)
-		.bindPopup("You are within " + radius + " meters from this point").openPopup();
+	var latlngs = [
+		$("locate")["coordinates"]
+	];
 
-	L.circle(e.latlng, radius).addTo(map);
-}
-
-map.on('locationfound', onLocationFound);
-
-function onLocationError(e) {
-	alert(e.message);
-}
-
-map.on('locationerror', onLocationError);
+};
 
 var latlngs = [
 	$("polyboard")["coordinates"]
@@ -43,32 +34,3 @@ var latlngs = [
 
 var polyline = L.polyline(latlngs, { color: 'red' }).addTo(map);
 map.fitBounds(polyline.getBounds());
-
-$('#Sub').click(function () {
-
-	$.ajax({
-		url: "CountryList.php",
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			countryBorders: $('#Countries').val()
-		},
-		success: function (result) {
-
-			console.log(JSON.stringify(result));
-
-			if (result.status.name == "ok") {
-
-				$('#Name').html(result['data'][0]['name']);
-				$('#Pop').html(result['data'][0]['population']);
-				$('#Cur').html(result['data'][0]['currency']);
-				$('#CW').html(result['data'][0]['weather']);
-				$('#WL').html(result['data'][0]['wiki_url']);
-
-			}
-
-		},
-
-	});
-
-});
