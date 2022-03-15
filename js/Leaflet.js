9955aac6baf52e906ccc50e79c8546b53c04fd15
Joshua <grimrai@protonmail.com>
2022-03-15 10:23:44 +0000

    Added change event

--- js/Leaflet.js
+++ js/Leaflet.js
@@ -16,11 +16,32 @@ function success(pos) {
 	console.log(`Latitude : ${crd.latitude}`);
 	console.log(`Longitude: ${crd.longitude}`);
 	console.log(`More or less ${crd.accuracy} meters.`);
-}
+
+	$.ajax({
+		url: "OpenCage.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			countryCode: $('#Countries').val()
+		},
+		success: function (result) {
+
+			console.log(JSON.stringify(result));
+
+			$('#Countries').change(function () {
+				if ($(this).val() == '1') {
+					$('#1').$('ISO_2');
+				}
+
+			},
+
+	});
+
+};
 
 navigator.geolocation.getCurrentPosition(success);
 
-function onLocationFound() {
+(function onLocationFound() {
 
 	$.ajax({
 		url: "OpenCage.php",
@@ -41,7 +62,7 @@ function onLocationFound() {
 
 	}); 
 
-};
+});
 
 var latlngs = [
 	$("polyboard")["coordinates"]
