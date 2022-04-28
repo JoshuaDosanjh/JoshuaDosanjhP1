6d9f3bb7083c65f92f8c6a3db01b551ae8a0c7e9
Joshua <grimrai@protonmail.com>
2022-04-28 15:30:04 +0100

    Change API

--- php/News.php
+++ php/News.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 
@@ -39,5 +39,33 @@ $decode = json_decode($result,true);
 header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode($output);
+*/
 
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+
+	$url = 'https://newsapi.org/v2/top-headlines?country=' . $_REQUEST['cCode'] . '&apiKey=ac7fbc9d08b44c25a1b9688d70bfbecd';
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);	
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output);
 ?>
